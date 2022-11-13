880a9115c919d7c447ac10409f2df69898955416
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-11-13 14:06:04 +0300

    Menu Created

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Carousel from "../../components/Carousel";
 import Campaigns from "../../components/Campaigns";
+import MenuWrapper from "../../components/product/MenuWrapper";
 const Index = () => {
   return (
     <div className="">
       <Carousel />
       <Campaigns />
+      <MenuWrapper />
     </div>
   );
 };
