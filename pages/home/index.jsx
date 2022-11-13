7a7b24d6e9a246cb3c55b17e925862b20b85aeaa
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-11-13 17:10:50 +0300

    Customers Created

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Carousel from "../../components/Carousel";
 import Campaigns from "../../components/Campaigns";
 import MenuWrapper from "../../components/product/MenuWrapper";
+import Customers from "../../components/customers/Customers";
 import About from "../../components/About";
 import Reservation from "../../components/Reservation";
 const Index = () => {
   return (
-    <div className="">
+    <React.Fragment>
       <Carousel />
       <Campaigns />
       <MenuWrapper />
       <About />
       <Reservation />
-    </div>
+      <Customers />
+    </React.Fragment>
   );
 };
 
