508f88138c2d692f09051c2a6bdafdaf080e864b
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-11-13 14:33:39 +0300

    About created

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Carousel from "../../components/Carousel";
 import Campaigns from "../../components/Campaigns";
 import MenuWrapper from "../../components/product/MenuWrapper";
+import About from "../../components/About";
 const Index = () => {
   return (
     <div className="">
       <Carousel />
       <Campaigns />
       <MenuWrapper />
+      <About />
     </div>
   );
 };
