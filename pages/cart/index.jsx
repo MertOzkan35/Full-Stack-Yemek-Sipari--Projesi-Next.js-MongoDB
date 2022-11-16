60752eda2b649dbf2df3c537b58b6330e1006e3a
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-11-16 19:53:55 +0300

    admin order manegment

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -6,22 +6,18 @@ import axios from "axios";
 import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
-
 const Cart = ({ userList }) => {
   const { data: session } = useSession();
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-
   const user = userList?.find((user) => user.email === session?.user?.email);
   const router = useRouter();
-
   const newOrder = {
     customer: user?.fullName,
     address: user?.address ? user?.address : "No address",
     total: cart.total,
     method: 0,
   };
-
   const createOrder = async () => {
     try {
       if (session) {
@@ -47,20 +43,35 @@ const Cart = ({ userList }) => {
       console.log(err);
     }
   };
-
   return (
     <div className="min-h-[calc(100vh_-_433px)]">
       <div className="flex justify-between items-center md:flex-row flex-col">
         <div className="md:min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-full">
           <table className="w-full text-sm text-center text-gray-500 min-w-[1000px]">
             <thead className="text-xs text-gray-400 uppercase bg-gray-700">
-              {cart.products.map((product) => (
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  PRODUCT
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  EXTRAS
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  PRICE
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  QUANTITY
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.products.map((product, index) => (
                 <tr
                   className="transition-all bg-secondary border-gray-700 hover:bg-primary"
-                  key={product._id}
+                  key={index}
                 >
                   <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white flex items-center gap-x-1 justify-center">
-                    <Image src="/images/f1.png" alt="" width={50} height={50} />
+                    <Image src={product?.img} alt="" width={50} height={50} />
                     <span>{product.name}</span>
                   </td>
                   <td className="py-4 px-6 font-medium whitespace-nowrap hover:text-white">
@@ -76,19 +87,16 @@ const Cart = ({ userList }) => {
                   </td>
                 </tr>
               ))}
-            </thead>
-            <tbody></tbody>
+            </tbody>
           </table>
         </div>
         <div className="bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center text-white p-12 md:w-auto w-full   md:text-start !text-center">
           <Title addClass="text-[40px]">CART TOTAL</Title>
-
           <div className="mt-6">
             <b>Subtotal: </b>${cart.total} <br />
             <b className=" inline-block my-1">Discount: </b>$0.00 <br />
             <b>Total: </b>${cart.total}
           </div>
-
           <div>
             <button
               className="btn-primary mt-4 md:w-auto w-52"
@@ -104,7 +112,6 @@ const Cart = ({ userList }) => {
 };
 export const getServerSideProps = async () => {
   const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users`);
-
   return {
     props: {
       userList: res.data ? res.data : [],
