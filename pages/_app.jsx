be361382bed1778fe9f0943f39485f985bd6d2be
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-11-17 18:55:41 +0300

    Route Change Progress

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,19 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../styles/globals.css";
 import Layout from "../layout/Layout";
-
-import { Provider } from "react-redux";
 import store from "../redux/store";
-
+import Router from "next/router";
+import NProgress from "nprogress";
+import { Provider } from "react-redux";
+import "../styles/globals.css";
+import "nprogress/nprogress.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { SessionProvider } from "next-auth/react";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
