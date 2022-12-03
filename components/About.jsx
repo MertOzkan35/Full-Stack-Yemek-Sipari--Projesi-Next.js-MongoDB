34454aaed1e59be422a23763ac73893179ff4937
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-12-03 19:34:53 +0300

    css fixed

--- components/About.jsx
+++ components/About.jsx
@@ -15,7 +15,7 @@ const About = () => {
           <p className="my-5 flex flex-col items-center">
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don`t look even slightly
+            humour, or randomised words which dont look even slightly
             believable. If you are going to use a passage of Lorem Ipsum, you
             need to be sure there isn`t anything embarrassing hidden in the
             middle of text. All
