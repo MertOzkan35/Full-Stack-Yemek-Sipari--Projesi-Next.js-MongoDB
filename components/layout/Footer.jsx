1589d83c11c1b01e8ac4b5770d2a8c32a92015d4
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-12-29 19:43:34 +0300

    css fixed

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -76,7 +76,7 @@ const Footer = () => {
           </div>
         </div>
         <p className="text-center mt-10">
-          © 2022 All Rights Reserved by Freee Html Templates
+          © 2022 All Rights Reserved by Freee Html Templates.
         </p>
       </div>
     </div>
