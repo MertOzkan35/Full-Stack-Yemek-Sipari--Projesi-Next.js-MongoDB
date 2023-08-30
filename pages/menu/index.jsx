e2c18451995c3a7b2e86ac7efdc1d6d97ccaefcb
Mert <107802530+MertOzkan35@users.noreply.github.com>
2023-08-30 13:29:55 +0300

    esdf

--- pages/menu/index.jsx
+++ pages/menu/index.jsx
@@ -26,3 +26,18 @@ export const getServerSideProps = async () => {
 };
 
 export default Index;
+
+// export const getServerSideProps = async () => {
+//   const category = await axios.get(
+//     `${process.env.NEXT_PUBLIC_API_URL}/categories`
+//   );
+//   const product = await axios.get(
+//     `${process.env.NEXT_PUBLIC_API_URL}/products`
+//   );
+//   return {
+//     props: {
+//       categoryList: category.data ? category.data : [],
+//       productList: product.data ? product.data : [],
+//     },
+//   };
+// };
